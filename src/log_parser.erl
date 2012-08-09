101c8b42df6c1e8e3edc96228d985ecb4bbf3c14
DisplayName <svart_ravn@vb.(none)>
2012-08-09 13:03:29 +0300

    Cosmetic changes

--- src/log_parser.erl
+++ src/log_parser.erl
@@ -1,22 +1,31 @@
 -module(log_parser).
 -behaviour(gen_server).
 
--export([start_link/0]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([
+        start_link/0,
+        parse/1
+]).
 
--export([parse/1]).
+-export([
+        init/1, 
+        handle_call/3, 
+        handle_cast/2, 
+        handle_info/2, 
+        terminate/2, 
+        code_change/3
+]).
 
 
 -include("logging.hrl").
 
 
--record(state, 
-          {server,
-           connection, 
-           db, 
-           collection, 
-           file_for_rejected_data
-          }).
+-record(state, {
+          server,
+          connection, 
+          db, 
+          collection, 
+          file_for_rejected_data
+}).
 
 
 
@@ -24,65 +33,65 @@
 %% APIs
 %% ===================================================================
 start_link() ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 
 parse(LogFileName) ->
-  gen_server:cast(?MODULE, {parse, LogFileName}).
+   gen_server:cast(?MODULE, {parse, LogFileName}).
 
 
 %% ===================================================================
 %% gen_server callbacks
 %% ===================================================================
 init([]) ->
-  {ok, MongoServer} = application:get_env(parse_mongo_logs, mongo_server),
-  {ok, MongoDb} = application:get_env(parse_mongo_logs, mongodb_dbname),
-  {ok, MongoCollection} = application:get_env(parse_mongo_logs, collection),
-  {ok, FileForRejectedData} = application:get_env(parse_mongo_logs, file_for_rejected_data),
+   {ok, MongoServer} = application:get_env(parse_mongo_logs, mongo_server),
+   {ok, MongoDb} = application:get_env(parse_mongo_logs, mongodb_dbname),
+   {ok, MongoCollection} = application:get_env(parse_mongo_logs, collection),
+   {ok, FileForRejectedData} = application:get_env(parse_mongo_logs, file_for_rejected_data),
 
-  case mongo:connect(MongoServer) of
-    {ok, Connection} ->
+   case mongo:connect(MongoServer) of
+      {ok, Connection} ->
       {ok, #state{server = MongoServer, connection = Connection, db = MongoDb, 
-              collection = MongoCollection, file_for_rejected_data = FileForRejectedData}};
-    {error, Reason} ->
-      ?log_error("Error Occured. Reason:~p. Connection settings: ~p~n", [Reason, MongoServer])
-  end.
+           collection = MongoCollection, file_for_rejected_data = FileForRejectedData}};
+      {error, Reason} ->
+         ?log_error("Error Occured. Reason:~p. Connection settings: ~p~n", [Reason, MongoServer])
+   end.
 
 
 handle_cast({parse, LogFileName}, State) ->
-  ?log_info("parsing started", []),
+   ?log_info("parsing started", []),
 
-  case file:open(LogFileName, [read]) of
-    {ok, Device} ->
-      save_into_file(State#state.file_for_rejected_data, ""),
-      parse_logfile_line_by_line(Device, "", State),
-      ?log_info("parsing ended", []),
-      file:close(Device),
+   case file:open(LogFileName, [read]) of
+      {ok, Device} ->
+         save_into_file(State#state.file_for_rejected_data, ""),
+         parse_logfile_line_by_line(Device, "", State),
+         ?log_info("parsing ended", []),
+         file:close(Device),
 
-      {noreply, State};
-    {error, Reason} -> 
-      ?log_error("Can't access file: ~p~n", [LogFileName]),
-      {stop, Reason, State}
-  end;
+         {noreply, State};
+      {error, Reason} -> 
+         ?log_error("Can't access file: ~p~n", [LogFileName]),
+         {stop, Reason, State}
+   end;
 
 handle_cast(Request, State) ->
-	{stop, {bad_arg, Request}, State}.
-	
+   {stop, {bad_arg, Request}, State}.
+  
 
 handle_info(Info, State) ->
-	{stop, {bad_arg, Info}, State}.
+   {stop, {bad_arg, Info}, State}.
 
 
 handle_call(_Request, _From, State) ->
-	{noreply, State}.
+   {noreply, State}.
 
 
 terminate(_Reason, _State) ->
-	ok.
+   ok.
 
 
 code_change(_OldSvn, State, _Extra) ->
-	{ok, State}.
+   {ok, State}.
 
 
 
@@ -94,7 +103,8 @@ code_change(_OldSvn, State, _Extra) ->
 %
 parse_logfile_line_by_line(Device, Accum, State) ->
    case io:get_line(Device, "") of
-      eof -> Accum;
+      eof -> 
+         ok;
       Line -> 
          {Term, UnhandledData} = try_to_get_term(Accum ++ Line, string:chr(Line, $.)),
          reinsert_message(State, Term),
@@ -108,16 +118,17 @@ parse_logfile_line_by_line(Device, Accum, State) ->
 try_to_get_term(Text, 0) -> {nothing, Text};
 
 try_to_get_term(Text, _Position) ->
-   Position = string:chr(Text, $.),
+   RefinedString = remove_spaces_and_brakes(Text),
+   Position = string:chr(RefinedString, $.),
 
-   Result = remove_spaces_and_brakes(string:substr(Text, Position + 1, length(Text) - Position)),
+   UnhandledData = string:substr(RefinedString, Position + 1, length(RefinedString) - Position),
 
-   StringToParse = remove_spaces_and_brakes(string:substr(Text, 1, Position)),
+   StringToParse = string:substr(RefinedString, 1, Position),
    {ok, Tokens, _EndLine} = erl_scan:string(StringToParse),
    {ok, Terms} = erl_parse:parse_term(Tokens),
    {Binary, _Trash} = Terms,
 
-   {binary_to_term(Binary), Result}.
+   {binary_to_term(Binary), UnhandledData}.
 
 
 %
@@ -125,10 +136,10 @@ try_to_get_term(Text, _Position) ->
 % when "" passed file should be cleansed
 %
 save_into_file(FileName, "") -> 
-  file:write_file(FileName, "", []);
+   file:write_file(FileName, "", []);
 
 save_into_file(FileName, Data) -> 
-  file:write_file(FileName, io_lib:fwrite("~p\n", [Data]), [append]).
+   file:write_file(FileName, io_lib:fwrite("~p\n", [Data]), [append]).
 
 
 %
@@ -145,11 +156,11 @@ reinsert_message(_State, nothing) -> ok;
 
 reinsert_message(_State = #state{connection = Connection, db = Db, collection = Collection}, 
                 _Data = {task, _RecipCnt, Msisdns, TranSid, _Tag, _Error}) ->
-  {ok, ok} = mongo:do(safe, master, Connection, Db, fun() ->
-     lists:foreach(fun(Msisdn) ->
-        ok = mongo:repsert(Collection, {'_id', Msisdn}, { '$push', { v, TranSid } })
+   {ok, ok} = mongo:do(safe, master, Connection, Db, fun() ->
+      lists:foreach(fun(Msisdn) ->
+         ok = mongo:repsert(Collection, {'_id', Msisdn}, { '$push', { v, TranSid } })
       end, Msisdns)
-    end);
+   end);
 
 reinsert_message(_State = #state{file_for_rejected_data = FileForRejectedData}, Data = {_Message, _UnpackedMessage}) ->  
-  save_into_file(FileForRejectedData, Data).
+   save_into_file(FileForRejectedData, Data).
--- src/parse_mongo_logs_app.erl
+++ src/parse_mongo_logs_app.erl
@@ -10,7 +10,6 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-	% application:start(mongodb),
    parse_mongo_logs_sup:start_link().
 
 stop(_State) ->
